0a460d4e0b6742e37cb77a0cde082d85a6226bb7
Nutbotty <nutbottykb@gmail.com>
2026-07-15 19:32:23 -0700

    Create hittable logic

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "nbrenderer.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-#include <iostream>
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
 
-
-color ray_color(const ray& r) {
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -21,6 +24,10 @@ int main() {
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
+    // WORLD
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // CAMERA
     auto focal_length = 1.0;
@@ -32,7 +39,7 @@ int main() {
     auto viewport_v = vec3(0, -viewport_height, 0);
 
     auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
 
     auto viewport_upper_left = camera_center - vec3(0,0, focal_length) - (viewport_u / 2) - (viewport_v / 2);
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
@@ -50,7 +57,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
