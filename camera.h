6531f85348074cb5a4ecf9f675c477699ed4df9c
Nutbotty <nutbottykb@gmail.com>
2026-07-15 22:01:14 -0700

    Implement gamma transformation

--- camera.h
+++ camera.h
@@ -98,7 +98,7 @@ private:
         if (world.hit(r, interval(0.001, infinity), rec)) {
             // vec3 direction = random_on_hemisphere(rec.normal); // non Lambert diffuse
             vec3 direction = rec.normal + random_unit_vector();   // Lambert diffuse
-            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+            return 0.3 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
--- color.h
+++ color.h
@@ -10,10 +10,21 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // transform from linear to gamma
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     //translate to byte range
     static const interval intensity(0.000, 0.999);
