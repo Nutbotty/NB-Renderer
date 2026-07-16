828077aff3c61e73dbd4b473be42d2fbe15c52e3
Nutbotty <nutbottykb@gmail.com>
2026-07-15 19:38:33 -0700

    Add interval.h

---
+++ interval.h
@@ -0,0 +1,35 @@
+//
+// Created by Nutbotty on 7/15/2026.
+//
+
+#ifndef NB_RENDERER_INTERVAL_H
+#define NB_RENDERER_INTERVAL_H
+#include "nbrenderer.h"
+
+class interval {
+    public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {}
+
+    interval(double min, double max) : min(min), max(max) {}
+
+    double size() const {
+        return max - min;
+    }
+
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    static const interval empty, universe;
+};
+
+const interval interval::empty    = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
+
+#endif //NB_RENDERER_INTERVAL_H
