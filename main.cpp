51ff3ec5aa792750185e1c10f9d2e40fdcbfd3b2
Nutbotty <nutbottykb@gmail.com>
2026-07-16 12:10:23 -0700

    Add dielectric support

--- main.cpp
+++ main.cpp
@@ -10,15 +10,19 @@ int main() {
 
     // WORLD
     hittable_list world;
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.6));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<dielectric>(1.5);
+    auto material_bubble_outside = make_shared<dielectric>(1.50);
+    auto material_bubble_inside = make_shared<dielectric>(1.00 / 1.50);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.3);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.5,    0.0, -1.5),   0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.1,    0.0, -1.25),   0.5, material_right));
+    world.add(make_shared<sphere>(point3( -1.0,    1.0, -1.4),   0.5, material_bubble_outside));
+    world.add(make_shared<sphere>(point3( -1.0,    1.0, -1.4),   0.4, material_bubble_inside));
+    world.add(make_shared<sphere>(point3(-1.5,    0.0, -1.8),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.2,    0.0, -1.25),   0.5, material_right));
 
     // CAMERA
     camera cam;
