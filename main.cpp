8caeb5fcf681ca735b20bb12cca212003e590d60
Nutbotty <nutbottykb@gmail.com>
2026-07-17 08:42:49 -0700

    Add depth-of-field support

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
 
     // WORLD
     hittable_list world;
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.6));
+    auto material_ground = make_shared<lambertian>(color(0.16, 0.16, 0.16));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.5);
     auto material_bubble_outside = make_shared<dielectric>(1.50);
@@ -31,6 +31,14 @@ int main() {
     cam.image_width  = 640;
     cam.samples_per_pixel = 20;
     cam.max_depth = 8;
+
+    cam.vfov = 37.85; // can convert to focal length (lens size)
+    cam.lookfrom = point3(-1,1,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 8.0;
+    cam.focus_dist    = 2.575;
 
     cam.render(world);
 }
