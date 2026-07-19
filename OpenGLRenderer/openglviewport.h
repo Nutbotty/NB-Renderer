b4b20a1b7629e2a0821d62be36165c6e8364b2f1
Nutbotty <nutbottykb@gmail.com>
2026-07-19 00:00:28 -0700

    Add openglviewport.h

---
+++ OpenGLRenderer/openglviewport.h
@@ -0,0 +1,15 @@
+//
+// Created by Nutbotty on 7/17/2026.
+//
+
+#ifndef NB_RENDERER_OPENGLVIEWPORT_H
+#define NB_RENDERER_OPENGLVIEWPORT_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "../Scene/scene.h"
+#include "editor_camera.h"
+
+EditorCameraState viewport(const Scene& scene);
+
+#endif //NB_RENDERER_OPENGLVIEWPORT_H
