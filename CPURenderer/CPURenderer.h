0416eca550f6f9d2273699cc50659fb8fdb117e1
Nutbotty <nutbottykb@gmail.com>
2026-07-19 11:34:19 -0700

    Add CPURenderer.h

---
+++ CPURenderer/CPURenderer.h
@@ -0,0 +1,17 @@
+//
+// Created by Nutbotty on 7/19/2026.
+//
+
+#ifndef NB_RENDERER_CPURENDERER_H
+#define NB_RENDERER_CPURENDERER_H
+
+#include "camera.h"
+
+class CPURenderer {
+    public:
+    CPURenderer () = default;
+
+    camera cam;
+};
+
+#endif //NB_RENDERER_CPURENDERER_H
