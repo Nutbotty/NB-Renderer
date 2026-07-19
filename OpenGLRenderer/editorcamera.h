356790ab034d9a1d395ad8ed2b388a006f22a83a
Nutbotty <nutbottykb@gmail.com>
2026-07-19 01:42:41 -0700

    Add DoF controls

--- OpenGLRenderer/editorcamera.h
+++ OpenGLRenderer/editorcamera.h
@@ -2,8 +2,8 @@
 // Created by Nutbotty on 7/17/2026.
 //
 
-#ifndef NB_RENDERER_CAMERA_H
-#define NB_RENDERER_CAMERA_H
+#ifndef NB_RENDERER_EDITORCAMERA_H
+#define NB_RENDERER_EDITORCAMERA_H
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
@@ -22,11 +22,13 @@ const float YAW         = -90.0f;
 const float PITCH       =  0.0f;
 const float SPEED       =  2.5f;
 const float SENSITIVITY =  0.1f;
-const float ZOOM        =  45.0f;
+const float ZOOM        =  20.0f;
+const float FOCUSDISTANCE = 10.0f;
+const float DEFOCUSANGLE = 15.0;
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
-class Camera
+class EditorCamera
 {
 public:
     // camera Attributes
@@ -43,29 +45,52 @@ public:
     float MouseSensitivity;
     float Zoom;
 
+    // depth of field
+    float FocusDistance;
+    float DefocusAngle;
+
     // constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    EditorCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH, float focusdistance = FOCUSDISTANCE, float defocusangle = DEFOCUSANGLE) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         Position = position;
         WorldUp = up;
         Yaw = yaw;
         Pitch = pitch;
         updateCameraVectors();
+        FocusDistance = focusdistance;
+        DefocusAngle = defocusangle;
     }
     // constructor with scalar values
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    EditorCamera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch, float focusdistance, float defocusangle) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         Position = glm::vec3(posX, posY, posZ);
         WorldUp = glm::vec3(upX, upY, upZ);
         Yaw = yaw;
         Pitch = pitch;
         updateCameraVectors();
+        FocusDistance = focusdistance;
+        DefocusAngle = defocusangle;
     }
 
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix()
     {
         return glm::lookAt(Position, Position + Front, Up);
+    }
+
+    glm::vec3 GetPosition() const
+    {
+        return Position;
+    }
+
+    glm::vec3 GetFront() const
+    {
+        return Front;
+    }
+
+    glm::vec3 GetUp() const
+    {
+        return Up;
     }
 
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
@@ -113,6 +138,8 @@ public:
         if (Zoom > 45.0f)
             Zoom = 45.0f;
     }
+
+
 
 private:
     // calculates the front vector from the Camera's (updated) Euler Angles
