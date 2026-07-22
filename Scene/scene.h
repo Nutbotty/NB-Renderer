84455adc530aae2b5519ef6673d3d9ddfc3e036c
Nutbotty <nutbottykb@gmail.com>
2026-07-22 12:01:58 -0700

    Create generic scene schema

--- Scene/scene.h
+++ Scene/scene.h
@@ -5,51 +5,103 @@
 #ifndef NB_RENDERER_SCENE_H
 #define NB_RENDERER_SCENE_H
 
-#include <memory>
 #include <vector>
+#include <cstdint>
+#include "../external/glm/glm/glm.hpp"
 
-#include "../hittable.h"
-#include "../nbrenderer.h"
+using MaterialId = std::uint32_t;
+using SphereId = std::uint32_t;
+
+enum class MaterialType : std::uint32_t
+{
+    Lambertian = 0,
+    Metal = 1,
+    Dielectric = 2
+};
+struct SceneMaterial {
+    MaterialType type = MaterialType::Lambertian;
+    glm::vec3 albedo{1.0f, 1.0f, 1.0f};
+    float fuzz = 0.0f;
+    float indexOfRefraction = 1.0f;
+};
+struct SceneSphere {
+    glm::vec3 center{0.0f};
+    float radius = 0.5f;
+    MaterialId material = 0;
+};
+struct SceneCamera {
+    glm::vec3 lookFrom{0.0f, 0.0f, 0.0f};
+    glm::vec3 lookAt{0.0f, 0.0f, -1.0f};
+    glm::vec3 up{0.0f, 1.0f, 0.0f};
+    float verticalFovDegrees = 40.0f;
+    float defocusAngleDegrees = 0.0f;
+    float focusDistance = 1.0f;
+};
+
 
 class Scene
 {
 public:
     Scene() = default;
 
-    void add(const std::shared_ptr<hittable>& object)
-    {
-        m_Objects.push_back(object);
-        m_Dirty = true;
+    MaterialId addMaterial(MaterialType type, const glm::vec3& albedo = glm::vec3(1.0f), float fuzz = 0.0f,float indexOfRefraction = 1.0f){
+        SceneMaterial material;
+        material.type = type;
+        material.albedo = albedo;
+        material.fuzz = fuzz;
+        material.indexOfRefraction =indexOfRefraction;
+        const auto id = static_cast<MaterialId>(m_Materials.size());
+        m_Materials.push_back(material);
+        MarkDirty();
+        return id;
+    }
+    SphereId addSphere(const glm::vec3& center, float radius, MaterialId material) {
+        SceneSphere sphere;
+        sphere.center = center;
+        sphere.radius = radius;
+        sphere.material = material;
+        const auto id = static_cast<SphereId>(m_Spheres.size());
+        m_Spheres.push_back(sphere);
+        MarkDirty();
+        return id;
+    }
+    void SetCamera(const SceneCamera& camera) {
+        m_Camera = camera;
+        MarkDirty();
     }
 
-    void Clear()
+    [[nodiscard]] const std::vector<SceneMaterial>& GetMaterials() const
     {
-        m_Objects.clear();
-        m_Dirty = true;
+        return m_Materials;
+    }
+    [[nodiscard]] const std::vector<SceneSphere>& GetSpheres() const
+    {
+        return m_Spheres;
+    }
+    [[nodiscard]] const SceneCamera& GetCamera() const
+    {
+        return m_Camera;
     }
 
-    const std::vector<std::shared_ptr<hittable>>& GetObjects() const
-    {
-        return m_Objects;
-    }
+    void Clear(){
+        m_Spheres.clear();
+        m_Materials.clear();
 
-    std::vector<std::shared_ptr<hittable>>& GetObjects()
-    {
-        return m_Objects;
+        MarkDirty();
     }
-
-    bool IsDirty() const
-    {
+    [[nodiscard]] bool IsDirty() const {
         return m_Dirty;
     }
-
-    void ClearDirty()
-    {
+    void MarkDirty() {
+        m_Dirty = true;
+    }
+    void ClearDirty() {
         m_Dirty = false;
     }
-
 private:
-    std::vector<std::shared_ptr<hittable>> m_Objects;
+    std::vector<SceneMaterial> m_Materials;
+    std::vector<SceneSphere> m_Spheres;
+    SceneCamera m_Camera;
 
     bool m_Dirty = true;
 };
