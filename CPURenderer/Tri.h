427524726c78f9e8d6e72367dbb815ab205fee0b
Nutbotty <nutbottykb@gmail.com>
2026-07-24 14:03:02 -0700

    Add Tri.h

---
+++ CPURenderer/Tri.h
@@ -0,0 +1,12 @@
+//
+// Created by Nutbotty on 7/24/2026.
+//
+
+#ifndef NB_RENDERER_TRI_H
+#define NB_RENDERER_TRI_H
+
+#include "hittable.h"
+#include "glm/detail/type_mat2x2.hpp"
+
+
+#endif //NB_RENDERER_TRI_H
