9f48852be6b4824a045d67ad35f3684b54c1907a
Nutbotty <nutbottykb@gmail.com>
2026-07-26 12:21:29 -0700

    Refactor to use SceneCamera as input

--- OpenGLRenderer/editorcamera.h
+++ OpenGLRenderer/editorcamera.h
@@ -7,153 +7,64 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include "../Scene/Scene.h"
 
-// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
-    RIGHT
+    RIGHT,
+    UP,
+    DOWN
 };
 
-// Default camera values
-const float YAW         = -90.0f;
-const float PITCH       =  0.0f;
-const float SPEED       =  2.5f;
-const float SENSITIVITY =  0.1f;
-const float ZOOM        =  20.0f;
-const float FOCUSDISTANCE = 10.0f;
-const float DEFOCUSANGLE = 15.0;
-
-
-// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
-class EditorCamera
-{
+class EditorCamera {
 public:
-    // camera Attributes
-    glm::vec3 Position;
-    glm::vec3 Front;
-    glm::vec3 Up;
-    glm::vec3 Right;
-    glm::vec3 WorldUp;
-    // euler Angles
-    float Yaw;
-    float Pitch;
-    // camera options
-    float MovementSpeed;
-    float MouseSensitivity;
-    float Zoom;
 
-    // depth of field
-    float FocusDistance;
-    float DefocusAngle;
+    glm::vec3 LookFrom{0.0f, 0.0f, 0.0f};
+    glm::vec3 LookAt{0.0f, 0.0f, -1.0f};
+    glm::vec3 VUp{0.0f, 1.0f, 0.0f};
 
-    // constructor with vectors
-    EditorCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH, float focusdistance = FOCUSDISTANCE, float defocusangle = DEFOCUSANGLE) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-    {
-        Position = position;
-        WorldUp = up;
-        Yaw = yaw;
-        Pitch = pitch;
-        updateCameraVectors();
-        FocusDistance = focusdistance;
-        DefocusAngle = defocusangle;
-    }
-    // constructor with scalar values
-    EditorCamera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch, float focusdistance, float defocusangle) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-    {
-        Position = glm::vec3(posX, posY, posZ);
-        WorldUp = glm::vec3(upX, upY, upZ);
-        Yaw = yaw;
-        Pitch = pitch;
-        updateCameraVectors();
-        FocusDistance = focusdistance;
-        DefocusAngle = defocusangle;
-    }
+    float VerticalFov = 40.0f;
+    float DefocusAngle = 0.0f;
+    float FocusDistance = 1.0f;
 
-    // returns the view matrix calculated using Euler Angles and the LookAt Matrix
-    glm::mat4 GetViewMatrix()
-    {
-        return glm::lookAt(Position, Position + Front, Up);
-    }
+    float MovementSpeed = 10.0f;
+    float MouseSensitivity = 0.1f;
 
-    glm::vec3 GetPosition() const
-    {
-        return Position;
-    }
 
-    glm::vec3 GetFront() const
-    {
+    EditorCamera() = default;
+    explicit EditorCamera(const SceneCamera& camera);
+    void SetFromSceneCamera(const SceneCamera& camera);
+
+    [[nodiscard]] SceneCamera ToSceneCamera() const;
+    [[nodiscard]] glm::mat4 GetViewMatrix() const;
+
+    void ProcessKeyboard(Camera_Movement direction,float deltaTime);
+    void ProcessMouseMovement(float xOffset, float yOffset);
+    void ProcessMouseScroll(float yOffset);
+
+    [[nodiscard]] const glm::vec3& GetFront() const {
         return Front;
     }
-
-    glm::vec3 GetUp() const
-    {
-        return Up;
+    [[nodiscard]] const glm::vec3& GetRight() const {
+        return Right;
     }
 
-    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
-    {
-        float velocity = MovementSpeed * deltaTime;
-        if (direction == FORWARD)
-            Position += Front * velocity;
-        if (direction == BACKWARD)
-            Position -= Front * velocity;
-        if (direction == LEFT)
-            Position -= Right * velocity;
-        if (direction == RIGHT)
-            Position += Right * velocity;
+    [[nodiscard]] const glm::vec3& GetCameraUp() const{
+        return CameraUp;
     }
-
-    // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
-    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
-    {
-        xoffset *= MouseSensitivity;
-        yoffset *= MouseSensitivity;
-
-        Yaw   += xoffset;
-        Pitch += yoffset;
-
-        // make sure that when pitch is out of bounds, screen doesn't get flipped
-        if (constrainPitch)
-        {
-            if (Pitch > 89.0f)
-                Pitch = 89.0f;
-            if (Pitch < -89.0f)
-                Pitch = -89.0f;
-        }
-
-        // update Front, Right and Up Vectors using the updated Euler angles
-        updateCameraVectors();
-    }
-
-    // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
-    void ProcessMouseScroll(float yoffset)
-    {
-        Zoom -= (float)yoffset;
-        if (Zoom < 1.0f)
-            Zoom = 1.0f;
-        if (Zoom > 45.0f)
-            Zoom = 45.0f;
-    }
-
-
 
 private:
-    // calculates the front vector from the Camera's (updated) Euler Angles
-    void updateCameraVectors()
-    {
-        // calculate the new Front vector
-        glm::vec3 front;
-        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        front.y = sin(glm::radians(Pitch));
-        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        Front = glm::normalize(front);
-        // also re-calculate the Right and Up vector
-        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up    = glm::normalize(glm::cross(Right, Front));
-    }
+    glm::vec3 Front{0.0f, 0.0f, -1.0f};
+    glm::vec3 Right{1.0f, 0.0f, 0.0f};
+    glm::vec3 CameraUp{0.0f, 1.0f, 0.0f};
+
+    float yaw = -90.0;
+    float pitch = 0.0f;
+
+     // lookFrom - lookAt
+    float TargetDistance = 1.0f;
 };
 
 #endif //NB_RENDERER_CAMERA_H
