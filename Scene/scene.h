0da0583c5e3b68d6af435ffa57ce7449edc0cf4c
Nutbotty <nutbottykb@gmail.com>
2026-07-31 15:04:06 -0700

    Add instances

--- Scene/scene.h
+++ Scene/scene.h
@@ -13,9 +13,9 @@ using MaterialId = std::uint32_t;
 using SphereId = std::uint32_t;
 using QuadId = std::uint32_t;
 using TriId = std::uint32_t;
+using InstanceId = std::uint32_t;
 
-enum class MaterialType : std::uint32_t
-{
+enum class MaterialType : std::uint32_t {
     Lambertian = 0,
     Metal = 1,
     Dielectric = 2
@@ -27,6 +27,12 @@ struct SceneMaterial {
     float indexOfRefraction = 1.0f;
     glm::vec3 emission{0.0, 0.0, 0.0};
     float emissionStrength = 0.0;
+};
+
+enum class ScenePrimitiveType : std::uint32_t {
+    Sphere = 0,
+    Quad = 1,
+    Triangle = 2
 };
 struct SceneSphere {
     glm::vec3 center{0.0f};
@@ -44,6 +50,14 @@ struct SceneTri {
     glm::vec3 U{1.0f, 0.0f, 0.0f};
     glm::vec3 V{0.0f, 1.0f, 0.0f};
     MaterialId material = 0;
+};
+struct ScenePrimitiveRecord {
+    ScenePrimitiveType type = ScenePrimitiveType::Triangle;
+    std::uint32_t index = 2;
+};
+struct SceneInstance {
+    ScenePrimitiveRecord primitive;
+    glm::mat4 objectToWorld{1.0f};
 };
 struct SceneCamera {
     glm::vec3 lookFrom{0.0f, 0.0f, 0.0f};
@@ -92,6 +106,7 @@ public:
         quad.material = material;
         const auto id = static_cast<QuadId>(m_Quads.size());
         m_Quads.push_back(quad);
+        MarkDirty();
         return id;
     }
     TriId addTri(const glm::vec3& Q, const glm::vec3& U, const glm::vec3& V, MaterialId material) {
@@ -102,6 +117,13 @@ public:
         tri.material = material;
         const auto id = static_cast<TriId>(m_Tris.size());
         m_Tris.push_back(tri);
+        MarkDirty();
+        return id;
+    }
+    InstanceId addInstance(ScenePrimitiveRecord primitive, const glm::mat4& transform) {
+        const auto id = static_cast<InstanceId>(m_instances.size());
+        m_instances.push_back(SceneInstance{primitive,transform});
+        MarkDirty();
         return id;
     }
     void SetCamera(const SceneCamera& camera) {
@@ -109,24 +131,22 @@ public:
         MarkDirty();
     }
 
-    [[nodiscard]] const std::vector<SceneMaterial>& GetMaterials() const
-    {
+    [[nodiscard]] const std::vector<SceneMaterial>& GetMaterials() const {
         return m_Materials;
     }
-    [[nodiscard]] const std::vector<SceneSphere>& GetSpheres() const
-    {
+    [[nodiscard]] const std::vector<SceneSphere>& GetSpheres() const {
         return m_Spheres;
     }
-    [[nodiscard]] const std::vector<SceneQuad>& GetQuads() const
-    {
+    [[nodiscard]] const std::vector<SceneQuad>& GetQuads() const {
         return m_Quads;
     }
-    [[nodiscard]] const std::vector<SceneTri>& GetTris() const
-    {
+    [[nodiscard]] const std::vector<SceneTri>& GetTris() const {
         return m_Tris;
     }
-    [[nodiscard]] const SceneCamera& GetCamera() const
-    {
+    [[nodiscard]] const std::vector<SceneInstance>& GetInstances() const {
+        return m_instances;
+    }
+    [[nodiscard]] const SceneCamera& GetCamera() const {
         return m_Camera;
     }
 
@@ -135,7 +155,7 @@ public:
         m_Quads.clear();
         m_Tris.clear();
         m_Materials.clear();
-
+        m_instances.clear();
         MarkDirty();
     }
     [[nodiscard]] bool IsDirty() const {
@@ -152,6 +172,7 @@ private:
     std::vector<SceneSphere> m_Spheres;
     std::vector<SceneQuad> m_Quads;
     std::vector<SceneTri> m_Tris;
+    std::vector<SceneInstance> m_instances;
     SceneCamera m_Camera;
 
     bool m_Dirty = true;
