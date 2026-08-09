a707ecab6fa3e37bde689fb8decea28d7339f77f
Nutbotty <nutbottykb@gmail.com>
2026-08-09 16:54:08 -0700

    Define Renderer class

--- Core/Renderer.h
+++ Core/Renderer.h
@@ -4,5 +4,23 @@
 
 #ifndef NB_RENDERER_RENDERER_H
 #define NB_RENDERER_RENDERER_H
+#include <cstdint>
+
+enum class RenderBackend {
+    OpenGL,
+    Vulkan
+};
+
+class Renderer {
+public:
+    virtual ~Renderer() = default;
+
+    virtual void Initialize(Window& window) = 0;
+    virtual void Exit() = 0;
+    virtual void SetScene(const Scene& scene) = 0;
+    virtual void Render(const Scene& scene, const EditorCamera& camera) = 0;
+    virtual void Resize(std::uint32_t width, std::uint32_t height) = 0;
+    virtual void ResetAccumulation();
+};
 
 #endif //NB_RENDERER_RENDERER_H
