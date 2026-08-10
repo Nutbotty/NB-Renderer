b2371caaa36be3abe2d24009d9a87a5c8efb08df
Nutbotty <nutbottykb@gmail.com>
2026-08-09 19:03:57 -0700

    Implement EditorLayer

--- Editor/Editor.h
+++ Editor/Editor.h
@@ -5,4 +5,19 @@
 #ifndef NB_RENDERER_EDITOR_H
 #define NB_RENDERER_EDITOR_H
 
+class EditorLayer {
+public:
+    void Initialize(Window& window, Renderer& renderer);
+
+    void Shutdown();
+    void BeginFrame();
+    void Update(Scene& scene, Renderer& renderer, float deltaTime);
+    void Render(Renderer& renderer);
+
+private:
+    void DrawMenu(Scene& scene, Renderer& renderer);
+    void DrawScenePanel(Scene& scene);
+    void DrawViewport(Renderer& renderer);
+};
+
 #endif //NB_RENDERER_EDITOR_H
