05bca35c682f88f1ab6bb67f8e900f0566e5c486
Nutbotty <nutbottykb@gmail.com>
2026-08-11 12:47:19 -0700

    Rename members

--- Renderer/Renderer.h
+++ Renderer/Renderer.h
@@ -5,6 +5,7 @@
 #ifndef NB_RENDERER_RENDERER_H
 #define NB_RENDERER_RENDERER_H
 #include <cstdint>
+#include <memory>
 #include "../Core/Window.h"
 #include "../Editor/editorcamera.h"
 
@@ -12,18 +13,33 @@ enum class RenderBackend {
     OpenGL,
     Vulkan
 };
+enum class RenderMode {
+    Editor,
+    Final
+};
+
+struct RenderSettings {
+    RenderMode mode = RenderMode::Editor;
+    std::uint32_t width = 1280;
+    std::uint32_t height = 720;
+    std::uint32_t maxSamples = 2048;
+    bool useFinalShader = false;
+};
 
 class Renderer {
 public:
     virtual ~Renderer() = default;
+    static std::unique_ptr<Renderer> Create(RenderBackend backend);
 
     virtual void Initialize(Window& window) = 0;
+    virtual void Shutdown() = 0;
     virtual void Exit() = 0;
     virtual void SetScene(const Scene& scene) = 0;
-    virtual void Render(const Scene& scene, const EditorCamera& camera) = 0;
+    virtual void Render(const Scene& scene, const EditorCamera& camera, const RenderSettings& settings) = 0;
     virtual void Resize(std::uint32_t width, std::uint32_t height) = 0;
     virtual void ResetAccumulation();
 
+    [[nodiscard]] virtual std::uint32_t GetAccumulationFrame() const = 0;
     [[nodiscard]] virtual RenderBackend GetBackend() const;
 };
 
