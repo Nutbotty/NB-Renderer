5bcf6ddadb78d82565e790267162efa5435ff5e6
Nutbotty <nutbottykb@gmail.com>
2026-08-11 19:17:18 -0700

    Add member variables

--- Editor/Editor.h
+++ Editor/Editor.h
@@ -4,27 +4,41 @@
 
 #ifndef NB_RENDERER_EDITOR_H
 #define NB_RENDERER_EDITOR_H
+#include <memory>
+
 #include "editorcamera.h"
+#include "ImGuiBackend.h"
+#include "../Core/Window.h"
+#include "../Renderer/Renderer.h"
+#include "../Scene/scene.h"
 
 class EditorLayer {
 public:
-    void Initialize(Window& window, Renderer& renderer);
+    EditorLayer() = default;
+    ~EditorLayer() = default;
 
-    [[nodiscard]] EditorCamera GetEditorCamera() const {
-        return m_camera;
-    }
-
+    void Initialize(Window& window, Renderer& renderer, const Scene& scene);
     void Shutdown();
     void BeginFrame();
     void Update(Scene& scene, Renderer& renderer, float deltaTime);
-    void Render(Renderer& renderer);
+    void Render();
+
+    [[nodiscard]] const EditorCamera& GetEditorCamera() const {
+        return m_Camera;
+    }
 
 private:
     void DrawMenu(Scene& scene, Renderer& renderer);
     void DrawScenePanel(Scene& scene);
     void DrawViewport(Renderer& renderer);
 
-    EditorCamera m_camera;
+private:
+    EditorCamera m_Camera;
+    std::unique_ptr<ImGuiBackend> m_ImGuiBackend;
+    bool m_Initialized = false;
+    bool m_ShowScenePanel = true;
+    bool m_ShowViewport = true;
+    bool m_ShowRendererPanel = true;
 };
 
 #endif //NB_RENDERER_EDITOR_H
