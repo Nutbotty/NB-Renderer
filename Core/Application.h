cd5856db2baf5fa36e045bd39b8d3b7193342ad7
Nutbotty <nutbottykb@gmail.com>
2026-08-11 18:57:26 -0700

    Add DeltaTime

--- Core/Application.h
+++ Core/Application.h
@@ -5,6 +5,7 @@
 #ifndef NB_RENDERER_APPLICATION_H
 #define NB_RENDERER_APPLICATION_H
 
+#include <chrono>
 #include <memory>
 
 #include "../Scene/scene.h"
@@ -17,16 +18,20 @@ public:
     ~Application();
 
     void Run();
+    float CalculateDeltaTime() {
+        const auto now = std::chrono::steady_clock::now();
+        const std::chrono::duration<float> elapsed = now - m_LastFrameTime;
+        m_LastFrameTime = now;
+        return std::min(elapsed.count(), 0.1f);
+    };
 
 private:
     Window m_Window;
     Scene m_Scene;
-
     std::unique_ptr<Renderer> m_Renderer;
-
-    // EditorLayer m_Editor;
-
+    EditorLayer m_Editor;
     bool m_Running = true;
+    std::chrono::steady_clock::time_point m_LastFrameTime;
 };
 
 #endif //NB_RENDERER_APPLICATION_H
