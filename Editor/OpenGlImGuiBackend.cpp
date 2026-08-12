e6e93f14b31ef59709ec3a9239136663e1cb96ca
Nutbotty <nutbottykb@gmail.com>
2026-08-12 00:54:29 -0700

    Add OpenGlImGuiBackend.cpp

---
+++ Editor/OpenGlImGuiBackend.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Nutbotty on 8/9/2026.
+//
+
+#include "OpenGLImGuiBackend.h"
+#include "../external/imgui/backends/imgui_impl_glfw.h"
+#include "../external/imgui/backends/imgui_impl_opengl3.h"
+
+#include "../Core/Window.h"
+
+void OpenGLImGuiBackend::Initialize(
+    Window& window
+)
+{
+    ImGui_ImplGlfw_InitForOpenGL(
+        window.GetNativeWindow(),
+        true
+    );
+
+    ImGui_ImplOpenGL3_Init(
+        "#version 430"
+    );
+}
+
+void OpenGLImGuiBackend::BeginFrame()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+
+    ImGui_ImplGlfw_NewFrame();
+}
+
+void OpenGLImGuiBackend::Render(
+    ImDrawData* drawData
+)
+{
+    ImGui_ImplOpenGL3_RenderDrawData(
+        drawData
+    );
+}
+
+void OpenGLImGuiBackend::Shutdown()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+
+    ImGui_ImplGlfw_Shutdown();
+}
