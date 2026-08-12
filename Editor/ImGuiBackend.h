b301b4039c731ad88521c884084b6b95a2606182
Nutbotty <nutbottykb@gmail.com>
2026-08-12 00:53:53 -0700

    Add ImGuiBackend class

--- Editor/ImGuiBackend.h
+++ Editor/ImGuiBackend.h
@@ -5,4 +5,24 @@
 #ifndef NB_RENDERER_IMGUIBACKEND_H
 #define NB_RENDERER_IMGUIBACKEND_H
 
+#pragma once
+
+#include <memory>
+#include "../Core/Window.h"
+#include "../external/imgui/imgui.h"
+#include "../Renderer/Renderer.h"
+
+
+struct ImDrawData;
+
+class ImGuiBackend {
+public:
+    virtual ~ImGuiBackend() = default;
+    static std::unique_ptr<ImGuiBackend> Create(RenderBackend api);
+    virtual void Initialize(Window& window) = 0;
+    virtual void BeginFrame() = 0;
+    virtual void Render(ImDrawData* drawData) = 0;
+    virtual void Shutdown() = 0;
+};
+
 #endif //NB_RENDERER_IMGUIBACKEND_H
