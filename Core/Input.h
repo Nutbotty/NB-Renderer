ca21284872f8193beb7e5366c55bbcda3925fd45
Nutbotty <nutbottykb@gmail.com>
2026-08-12 19:15:36 -0700

    Expand camera controls

--- Core/Input.h
+++ Core/Input.h
@@ -14,30 +14,27 @@ public:
     explicit Input(EditorCamera& camera);
 
     void Initialize(GLFWwindow* window);
-    void Update(float deltaTime);
+    void Update(float deltaTime, bool viewportHovered, bool viewportFocused);
+
+    [[nodiscard]] bool IsCameraLookActive() const {
+        return m_CameraLookActive;
+    }
 
 private:
-    static void MouseCallback(
-        GLFWwindow* window,
-        double xpos,
-        double ypos
-    );
+    static void MouseCallback(GLFWwindow* window, double xpos, double ypos);
+    static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+    void BeginCameraLook();
+    void EndCameraLook();
 
-    static void ScrollCallback(
-        GLFWwindow* window,
-        double xoffset,
-        double yoffset
-    );
-
-private:
     EditorCamera& m_Camera;
-
+    bool m_CameraLookActive = false;
     GLFWwindow* m_Window = nullptr;
-
     bool m_FirstMouse = true;
-
     float m_LastX = 0.0f;
     float m_LastY = 0.0f;
+    float m_MouseDeltaX = 0.0f;
+    float m_MouseDeltaY = 0.0f;
+    float m_ScrollDelta = 0.0f;
 };
 
 #endif //NB_RENDERER_INPUT_H
