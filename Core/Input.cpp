50345637d6d7be9d5fa94de146f80c0f63beeee1
Nutbotty <nutbottykb@gmail.com>
2026-08-12 19:25:08 -0700

    Update camera controls

--- Core/Input.cpp
+++ Core/Input.cpp
@@ -6,54 +6,63 @@
 
 Input::Input(EditorCamera& camera) : m_Camera(camera) {}
 
-void Input::Initialize(GLFWwindow* window)
-{
+void Input::Initialize(GLFWwindow* window) {
     m_Window = window;
-
     glfwSetWindowUserPointer(window, this);
-
     glfwSetCursorPosCallback(window, MouseCallback);
     glfwSetScrollCallback(window, ScrollCallback);
-
-    glfwSetInputMode(
-        window,
-        GLFW_CURSOR,
-        GLFW_CURSOR_DISABLED
-    );
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
 
-void Input::Update(float deltaTime)
-{
-    if (glfwGetKey(m_Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(m_Window, true);
+void Input::Update(float deltaTime, bool viewportHovered, bool viewportFocused) {
+    const bool rightMouseDown = glfwGetMouseButton(m_Window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
 
-    if (glfwGetKey(m_Window, GLFW_KEY_W) == GLFW_PRESS)
-        m_Camera.ProcessKeyboard(FORWARD, deltaTime);
-
-    if (glfwGetKey(m_Window, GLFW_KEY_S) == GLFW_PRESS)
-        m_Camera.ProcessKeyboard(BACKWARD, deltaTime);
-
-    if (glfwGetKey(m_Window, GLFW_KEY_A) == GLFW_PRESS)
-        m_Camera.ProcessKeyboard(LEFT, deltaTime);
-
-    if (glfwGetKey(m_Window, GLFW_KEY_D) == GLFW_PRESS)
-        m_Camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (!m_CameraLookActive && rightMouseDown && viewportHovered) {
+        BeginCameraLook();
+    }
+    if (m_CameraLookActive && !rightMouseDown) {
+        EndCameraLook();
+    }
+    if (m_CameraLookActive) {
+        if (m_MouseDeltaX != 0.0f || m_MouseDeltaY != 0.0f) {
+            m_Camera.ProcessMouseMovement(m_MouseDeltaX, m_MouseDeltaY);
+        }
+        if (glfwGetKey(m_Window, GLFW_KEY_W) == GLFW_PRESS) {
+            m_Camera.ProcessKeyboard(FORWARD, deltaTime);
+        }
+        if (glfwGetKey(m_Window, GLFW_KEY_S) == GLFW_PRESS) {
+            m_Camera.ProcessKeyboard(BACKWARD, deltaTime);
+        }
+        if (glfwGetKey(m_Window, GLFW_KEY_A) == GLFW_PRESS) {
+            m_Camera.ProcessKeyboard(LEFT, deltaTime);
+        }
+        if (glfwGetKey(m_Window, GLFW_KEY_D) == GLFW_PRESS) {
+            m_Camera.ProcessKeyboard(RIGHT, deltaTime);
+        }
+    }
+    if (viewportHovered && m_ScrollDelta != 0.0f) {
+        m_Camera.ProcessMouseScroll(m_ScrollDelta);
+    }
+    m_MouseDeltaX = 0.0f;
+    m_MouseDeltaY = 0.0f;
+    m_ScrollDelta = 0.0f;
 }
 
-void Input::MouseCallback(
-    GLFWwindow* window,
-    double xposIn,
-    double yposIn
-)
-{
-    auto* input =
-        static_cast<Input*>(glfwGetWindowUserPointer(window));
-
+void Input::MouseCallback(GLFWwindow* window, double xposIn, double yposIn) {
+    auto* input = static_cast<Input*>(glfwGetWindowUserPointer(window));
+    if (!input) {
+        return;
+    }
     float xpos = static_cast<float>(xposIn);
     float ypos = static_cast<float>(yposIn);
 
-    if (input->m_FirstMouse)
-    {
+    if (!input->m_CameraLookActive) {
+        input->m_LastX = xpos;
+        input->m_LastY = ypos;
+        input->m_FirstMouse = true;
+        return;
+    }
+    if (input->m_FirstMouse) {
         input->m_LastX = xpos;
         input->m_LastY = ypos;
         input->m_FirstMouse = false;
@@ -64,23 +73,36 @@ void Input::MouseCallback(
 
     input->m_LastX = xpos;
     input->m_LastY = ypos;
-
-    input->m_Camera.ProcessMouseMovement(
-        xoffset,
-        yoffset
-    );
+    input->m_MouseDeltaX += xoffset;
+    input->m_MouseDeltaY += yoffset;
 }
 
-void Input::ScrollCallback(
-    GLFWwindow* window,
-    double,
-    double yoffset
-)
-{
-    auto* input =
-        static_cast<Input*>(glfwGetWindowUserPointer(window));
+void Input::ScrollCallback(GLFWwindow* window, double, double yoffset) {
+    auto* input = static_cast<Input*>(glfwGetWindowUserPointer(window));
+    if (!input) {
+        return;
+    }
+    input->m_ScrollDelta += static_cast<float>(yoffset);
+}
 
-    input->m_Camera.ProcessMouseScroll(
-        static_cast<float>(yoffset)
-    );
+void Input::BeginCameraLook() {
+    if (m_CameraLookActive) {
+        return;
+    }
+    m_CameraLookActive = true;
+    m_MouseDeltaX = 0.0f;
+    m_MouseDeltaY = 0.0f;
+    m_FirstMouse = true;
+    glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+void Input::EndCameraLook() {
+    if (!m_CameraLookActive) {
+        return;
+    }
+    m_CameraLookActive = false;
+    m_MouseDeltaX = 0.0f;
+    m_MouseDeltaY = 0.0f;
+    m_FirstMouse = true;
+    glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
