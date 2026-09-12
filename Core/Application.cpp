0c7af75e838e4bf65329fa8a27abc959c57bd3d0
Nutbotty <nutbottykb@gmail.com>
2026-09-12 00:26:11 -0700

    Add final render check to loop

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -38,6 +38,10 @@ void Application::Run() {
         m_Editor.BeginFrame();
         m_Editor.Update(m_Scene, *m_Renderer, deltaTime);
         m_Renderer->Render(m_Scene, m_Editor.GetEditorCamera(), m_Editor.GetViewportRenderSettings());
+        if (m_Editor.ConsumeFinalRenderRequest()) {
+            m_Renderer->RenderFinal(m_Scene, m_Editor.GetEditorCamera(),
+                m_Editor.GetFinalRenderSettings(), m_Editor.GetFinalRenderPath());
+        }
         m_Editor.Render();
         m_Renderer->Present();
     }
